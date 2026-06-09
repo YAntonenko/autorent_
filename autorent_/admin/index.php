a5b8f7c4b04e34112e4362b8eca5f47d953569ae
Yuna <yuna@hkhk.edu.ee>
2026-06-09 19:26:46 +0300

    Refactor car management table display

--- autorent_/admin/index.php
+++ autorent_/admin/index.php
@@ -1,39 +1,45 @@
-<?php include '_admin_header.php'; ?>
+<?php
+include "_admin_header.php";
 
-<div class="container py-5">
-    <div class="d-flex justify-content-between mb-3">
-        <h1>Autode haldus</h1>
-        <a href="add_car.php" class="btn btn-success">Lisa auto</a>
-    </div>
+$result = mysqli_query($conn, "SELECT * FROM cars");
+?>
 
-    <table class="table table-bordered table-striped bg-white">
+<h1>Autode haldus</h1>
+
+<a href="add_car.php" class="btn btn-success mb-3">Lisa auto</a>
+
+<table class="table table-bordered bg-white">
+    <tr>
+        <th>ID</th>
+        <th>Auto</th>
+        <th>Hind</th>
+        <th>Tegevused</th>
+    </tr>
+
+    <?php while ($car = mysqli_fetch_assoc($result)) { ?>
         <tr>
-            <th>ID</th>
-            <th>Pilt</th>
-            <th>Auto</th>
-            <th>Aasta</th>
-            <th>Hind</th>
-            <th>Staatus</th>
-            <th>Tegevused</th>
-        </tr>
-        <?php
-        $result = mysqli_query($conn, "SELECT * FROM cars ORDER BY id DESC");
-        while ($car = mysqli_fetch_assoc($result)) {
-        ?>
-            <tr>
-                <td><?php echo $car['id']; ?></td>
-                <td><img src="<?php echo $car['image']; ?>" width="90"></td>
-                <td><?php echo $car['brand'] . ' ' . $car['model']; ?></td>
-                <td><?php echo $car['year']; ?></td>
-                <td><?php echo $car['price_per_day']; ?> €</td>
-                <td><?php echo $car['status']; ?></td>
-                <td>
-                    <a href="edit_car.php?id=<?php echo $car['id']; ?>" class="btn btn-warning btn-sm">Muuda</a>
-                    <a href="delete_car.php?id=<?php echo $car['id']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Kas kustutan?')">Kustuta</a>
-                </td>
-            </tr>
-        <?php } ?>
-    </table>
-</div>
+            <td><?php echo $car["id"]; ?></td>
 
-<?php include '_admin_footer.php'; ?>
+            <td>
+                <?php echo $car["brand"]; ?>
+                <?php echo $car["model"]; ?>
+            </td>
+
+            <td>
+                <?php echo $car["price_per_day"]; ?> €
+            </td>
+
+            <td>
+                <a href="edit_car.php?id=<?php echo $car["id"]; ?>" class="btn btn-warning btn-sm">
+                    Muuda
+                </a>
+
+                <a href="delete_car.php?id=<?php echo $car["id"]; ?>" class="btn btn-danger btn-sm">
+                    Kustuta
+                </a>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
+
+<?php include "_admin_footer.php"; ?>
